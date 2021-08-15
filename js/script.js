1f577e5be86fd7a91cd9bd6f0ff2edbb34be53e3
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2021-08-15 21:51:32 +0200

    slides and dots are working

--- js/script.js
+++ js/script.js
@@ -76,6 +76,7 @@ const initCarousel = () => {
   const nextButton = document.getElementById("right");
   const dotsNav = document.querySelector(".carousel__nav");
   const dots = Array.from(dotsNav.children);
+
   // init first dot
   dots[0].classList.add("current-slide");
   slides[0].classList.add("current-slide");
@@ -91,14 +92,20 @@ const initCarousel = () => {
   nextButton.addEventListener("click", (e) => {
     const currentSlide = track.querySelector(".current-slide");
     const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector(".current-slide");
+    const nextDot = currentDot.nextElementSibling;
     moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
   });
 
   // Move slides left
   prevButton.addEventListener("click", (e) => {
     const currentSlide = track.querySelector(".current-slide");
     const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector(".current-slide");
+    const prevDot = currentDot.previousElementSibling;
     moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
   });
 
   // map the arrow keys to also move the slides
@@ -107,22 +114,47 @@ const initCarousel = () => {
     const currentSlide = track.querySelector(".current-slide");
     const prevSlide = currentSlide.previousElementSibling;
     const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector(".current-slide");
+    const prevDot = currentDot.previousElementSibling;
+    const nextDot = currentDot.nextElementSibling;
     switch (key) {
       case "ArrowLeft":
         moveToSlide(track, currentSlide, prevSlide);
+        updateDots(currentDot, prevDot);
         break;
       case "ArrowRight":
         moveToSlide(track, currentSlide, nextSlide);
+        updateDots(currentDot, nextDot);
         break;
     }
   });
 
   // When I click the nav indicator then move to that slide
+  dotsNav.addEventListener("click", (e) => {
+    // e has a ton of info on what we're clicking on
+    // I don't want this to run when I don't click on a dot
+    const targetDot = e.target.closest("button");
+    if (!targetDot) return;
+    const currentSlide = track.querySelector(".current-slide");
+    const currentDot = dotsNav.querySelector(".current-slide");
+    const targetIndex = dots.findIndex((dot) => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+  });
 };
 
+// general slide mover
 const moveToSlide = (track, currentSlide, targetSlide) => {
   const amountToMove = targetSlide.style.left;
   track.style.transform = `translateX(-${amountToMove})`;
   currentSlide.classList.remove("current-slide");
   targetSlide.classList.add("current-slide");
+};
+
+// update the dots' style
+const updateDots = (currentDot, targetDot) => {
+  currentDot.classList.remove("current-slide");
+  targetDot.classList.add("current-slide");
 };
